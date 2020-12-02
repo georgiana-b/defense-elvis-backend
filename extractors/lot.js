2b3f89a435dd07b3c6dfceffc925324fafb13811
georgiana <georgiana@localhost.localdomain>
2020-12-02 18:29:20 +0100

    Remove default for bid

--- extractors/lot.js
+++ extractors/lot.js
@@ -15,7 +15,7 @@ function extractLot(lotAttrs, tenderAttrs) {
     description: lotAttrs.description,
     contractNumber: lotAttrs.contractNumber,
     lotNumber: lotAttrs.lotNumber,
-    bidsCount: lotAttrs.bidsCount || bids.length,
+    bidsCount: lotAttrs.bidsCount,
     validBidsCount: lotAttrs.validBidsCount,
     awardDecisionDate: lotAttrs.awardDecisionDate,
     awardCriteria: lotAttrs.awardCriteria || tenderAttrs.awardCriteria,
